179bc21e9233656402cc400cbb525ed35ea672e2
Alex Gonzales <acgonzales42@gmail.com>
2023-11-27 15:05:52 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { GameDetails } from './types';
 
 function App() {
-  
+
   const gameDetails: GameDetails = {
     isOngoing: false,
     gameMode: 1,
